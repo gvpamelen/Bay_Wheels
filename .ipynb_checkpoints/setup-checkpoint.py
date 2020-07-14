2a26bcf96361fb54e10e41e6ee11946fc2d4f1c6
“gijsvanpamelen” <g.v.pamelen@gmail.com>
2020-07-14 23:27:42 +0200

    plots for report

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploration Bay Wheels dataset 01/2020',
+    author='Gijs van Pamelen',
+    license='MIT',
+)
